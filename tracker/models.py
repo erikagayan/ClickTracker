598fe11fb23ce1cae241b9f512383bb079ad2435
Erik <erik.agayan1@gmail.com>
2024-07-30 14:52:01 +0300

    Add models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Click(models.Model):
+    ip_address = models.GenericIPAddressField(protocol="both")
+    user_agent = models.CharField(max_length=256)  # store browser info
+    timestamp = models.DateTimeField(auto_now_add=True)  # store click time
+    project_id = models.CharField(max_length=100, blank=True, null=True)
+    thank_you_page = models.URLField(default="http://example.com/thank-you")
+
+    def __str__(self):
+        return f"Click from {self.ip_address} at {self.timestamp}"
